09d2ac76eae3cba1eed61be1cd6c7ca12c3732a9
George Huliaris <georgehuliaris@me.com>
2021-03-22 20:58:26 -0500

    added javascript for geolocation

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 $(document).ready(function() {
 
-    function weather() {
-        var URL = "api.openweathermap.org/data/2.5/weather?q={city name}&appid=7d110956aa512dec539ae86b6c7a578f";
+    // get location of user 
+    navigator.geolocation.getCurrentPosition(success, error);
+// if user location is accessed
+    function success(pos) {
+        var lat = pos.coords.latitude;
+        var long = pos.coords.longitude;
+        weather(lat, long)
+    }
+// if user location unsuccessful
+    function error() {
+        console.log("error");
+    }
+
+    function weather(lat, long) {
+        var URL = "https://api.openweathermap.org/data/2.5/weather?q=houston&appid=7d110956aa512dec539ae86b6c7a578f";
 
         $.getJSON(URL, function(data) { 
             console.log(data)
@@ -9,19 +22,17 @@ $(document).ready(function() {
         });
     }
 
-    weather();
-
     function updateDOM(data) {
 
         var city = data.name;
         var temp = Math.round(data.main.temp);
         var desc = data.weather[0].description;
+        var icon = data.weather[0].icon;
 
         $("#city").html(city)
         $("#temp").html(temp);
-        $("#desc").html(desc)
+        $("#desc").html(desc); 
         $("icon").attr("src", icon);
     }
 
 });
-
